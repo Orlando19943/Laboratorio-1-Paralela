1830f65c4b2e19ec33ac4bece0596584a3e7def4
Orlando <orlando.osberto@gmail.com>
2023-02-20 16:47:31 -0600

    Subiendo las partes que faltaban del ejercicio 2

---
+++ Ejercicio 2/Ejercicio d y e/piSeriesSeq.c
@@ -0,0 +1,49 @@
+/* File:     piSeriesNaive.c
+ * Purpose:  Estimate pi using OpenMP and the formula
+ *              pi = 4*[1 - 1/3 + 1/5 - 1/7 + 1/9 - . . . ]
+ *
+ * Compile:  gcc -g -Wall -fopenmp -o piSeriesNaive piSeriesNaive.c -lm
+ * Run:      piSeriesNaive <thread_count> <n>
+ *           thread_count is the number of threads
+ *           n is the number of terms of the series to use
+ *
+ * Output:   The estimate of pi and the value of pi computed by the
+ *           arctan function in the math library
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+void Usage(char *prog_name);
+
+int main(int argc, char *argv[])
+{
+    long long n, k;
+    int thread_count;
+    double sum = 0.0;
+    double factor = 1.0;
+
+    thread_count = strtol(argv[1], NULL, 10);
+    n = strtoll(argv[2], NULL, 10);
+
+    double first_time = omp_get_wtime();
+    for (k = 0; k < n; k++) {
+        sum += factor/(2*k+1);
+        factor = -1.0* factor;
+    }
+    sum = 4.0*sum;
+    double second_time = omp_get_wtime();
+    // write time on file
+    FILE *fp;
+    fp = fopen("timeA.txt", "a");
+    fprintf(fp, "Tiempo secuencial %f con valor de pi: %.14f\n", second_time - first_time,sum);
+    fclose(fp);
+    printf("With n = %lld terms and %d threads,\n", n, thread_count);
+    printf("   Our estimate of pi = %.14f\n", sum);
+    printf("                   PI = %.14f\n", 4.0 * atan(1.0));
+    printf("Time: %f\n", second_time - first_time);
+
+    return 0;
+} /* main */
